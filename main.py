8600efc54e5ba68bc84ae317dd0a4301b4a307f5
Will Trimble <wmt1276@rit.edu>
2023-07-09 13:52:57 -0400

    Plot Buffers Around Hydrants

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import geopandas
 import matplotlib.pyplot as plt
 
+def plot(threshold, gs, base):
+    buffer = gs.buffer(threshold[0])
+    return buffer.plot(ax = base, color=threshold[1])
 
-gdf = geopandas.read_file("data/export.geojson")
+def main():
 
-print(gdf)
+    hydrants = geopandas.read_file("data/agency11634_locations_1688920154565.csv")
+    hydrant_geometries = geopandas.GeoDataFrame(
+        hydrants, geometry=geopandas.points_from_xy(hydrants.lon, hydrants.lat), crs="WGS 84"
+    )
+    hydrant_geometries.to_crs("26916", inplace = True)
+    hydrant_series = hydrant_geometries.centroid
+    
+    base = None
+    thresholds = [[304.8, 'yellow'], [152.4, 'red']]
+    for threshold in thresholds:
+        base = plot(threshold, hydrant_series, base)
 
-gdf.plot()
-plt.show()
+    plt.show()
+
+if __name__ == "__main__":
+    main()
