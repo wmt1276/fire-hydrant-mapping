b1d66358181765fbc27c2f7e3da591c51a267ec3
Will Trimble <wmt1276@rit.edu>
2023-07-09 23:12:03 -0400

    Added Roads

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import geopandas
 import matplotlib.pyplot as plt
 
-def plot(threshold, gs, base):
+def plot(threshold, gs, base, layer):
     buffer = gs.buffer(threshold[0])
-    return buffer.plot(ax = base, color=threshold[1])
+    return buffer.plot(ax = base, color=threshold[1], zorder = layer)
 
 def main():
-
+    roads = geopandas.read_file("data/tl_rd22_18145_roads")
+    roads.to_crs("26916", inplace = True)
+    
     hydrants = geopandas.read_file("data/agency11634_locations_1688920154565.csv")
     hydrant_geometries = geopandas.GeoDataFrame(
         hydrants, geometry=geopandas.points_from_xy(hydrants.lon, hydrants.lat), crs="WGS 84"
@@ -14,10 +16,12 @@ def main():
     hydrant_geometries.to_crs("26916", inplace = True)
     hydrant_series = hydrant_geometries.centroid
     
-    base = None
     thresholds = [[304.8, 'yellow'], [152.4, 'red']]
-    for threshold in thresholds:
-        base = plot(threshold, hydrant_series, base)
+    
+    layers = len(thresholds)
+    base = roads.plot(color="gray", zorder=layers)
+    for threshold in range(layers):
+        base = plot(thresholds[threshold], hydrant_series, base, threshold)
 
     plt.show()
 
