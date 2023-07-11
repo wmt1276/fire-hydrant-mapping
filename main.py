4bd07b183807209ee69d41e04b8d601a6a8eb784
Will Trimble <wmt1276@rit.edu>
2023-07-10 22:10:31 -0400

    Combine Active911 and OSM Hydrants

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 import geopandas
+import pandas
 import matplotlib.pyplot as plt
 
 def plot(threshold, gs, base, layer):
-    buffer = gs.buffer(threshold[0])
-    return buffer.plot(ax = base, color=threshold[1], zorder = layer)
+    envelope = gs.buffer(threshold[0])
+    return envelope.plot(ax = base, color=threshold[1], zorder = layer)
 
 def main():
     roads = geopandas.read_file("data/tl_rd22_18145_roads")
     roads.to_crs("26916", inplace = True)
     
-    hydrants = geopandas.read_file("data/agency11634_locations_1688920154565.csv")
-    hydrant_geometries = geopandas.GeoDataFrame(
-        hydrants, geometry=geopandas.points_from_xy(hydrants.lon, hydrants.lat), crs="WGS 84"
+    active_911_hydrants = geopandas.read_file("data/agency11634_locations_1688920154565.csv")
+    active_911_hydrant_geometries = geopandas.GeoDataFrame(
+        active_911_hydrants, geometry=geopandas.points_from_xy(active_911_hydrants.lon, active_911_hydrants.lat), crs="WGS 84"
     )
-    hydrant_geometries.to_crs("26916", inplace = True)
-    hydrant_series = hydrant_geometries.centroid
+    active_911_hydrant_geometries.to_crs("26916", inplace = True)
+    active_911_series = active_911_hydrant_geometries.centroid
+    
+    osm_hydrants = geopandas.read_file("data/export.geojson")
+    osm_hydrants.to_crs("26916", inplace = True)
+    osm_series = osm_hydrants.geometry
+    hydrant_series = pandas.concat([active_911_series, osm_series])
+    
+    print(hydrant_series)
     
     thresholds = [[304.8, 'yellow'], [152.4, 'red']]
     
